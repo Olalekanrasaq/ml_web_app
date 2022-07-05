d51744a5c17b4f9186ce217d7cbf2a051eab06a9
Olalekan Rasaq <69814366+Olalekanrasaq@users.noreply.github.com>
2022-07-05 15:43:50 +0100

    Update ml_web_app.py

--- ml_web_app.py
+++ ml_web_app.py
@@ -151,12 +151,14 @@ test_inputs = test_inputs[num_cols + encoded_cols]
 # model
 select_model = st.sidebar.selectbox('Choose a Classifier model', ('None', 'Random Forest', 'Decision Tree'))
 
+@st.cache
 def model_acc(inputs, targets, name=''):
     model.fit(inputs, targets)
     preds = model.predict(inputs)
     accuracy = accuracy_score(targets, preds)
     return accuracy
 
+@st.cache
 def get_params(s_model):
     params = dict()
     if s_model == 'Random Forest':
@@ -188,6 +190,7 @@ if params:
         st.markdown('#### Model Accuracy Score')
         st.write(f'**Accuracy: {acc*100:.2f}**')
 
+@st.cache
 def predict_input(new_input):
     input_df = pd.DataFrame([new_input])
     numeric_cols = input_df.select_dtypes(include=np.number).columns.tolist()
