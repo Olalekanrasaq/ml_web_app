fc63ba1c82ac6da28f7712f59bb74739e46f7534
Olalekan Rasaq <69814366+Olalekanrasaq@users.noreply.github.com>
2022-07-05 15:23:53 +0100

    Update ml_web_app.py

--- ml_web_app.py
+++ ml_web_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-import plotly.express as px
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.impute import SimpleImputer
